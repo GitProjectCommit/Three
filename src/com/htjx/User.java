024076b3591a1b812360d73eb6f37522ce507b67
GitProjectCommit <2690164089@qq.com>
2016-06-13 16:24:49 +0800

    6
    6

--- src/com/htjx/User.java
+++ src/com/htjx/User.java
@@ -6,6 +6,8 @@ public class User {
 		System.out.println("001");
 			System.out.println("002");
 			System.out.println("003");
+			
 			System.out.println("005");
+			System.out.println("006");
 	}
 }
